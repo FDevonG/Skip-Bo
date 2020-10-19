13069a22c9813bfbcbb73367094974aa1a4c9c3c
Devon Gillis <39134971+FDevonG@users.noreply.github.com>
2020-10-19 19:13:58 -0300

    friends list fixed

--- Assets/Scripts/FreindsPanel.cs
+++ Assets/Scripts/FreindsPanel.cs
@@ -10,40 +10,58 @@ public class FreindsPanel : MonoBehaviour
     List<GameObject> friendPanels = new List<GameObject>();
 
     private void OnEnable() {
+        LoadingScreen.Instance.TurnOnLoadingScreen();
         StartCoroutine(BuildFriends());
     }
 
     private IEnumerator BuildFriends() {
         DeleteFriends();
-        PhotonNetwork.Friends = null;
-        PhotonNetwork.FindFriends(LocalUser.locUser.friends.ToArray());
-        yield return new WaitUntil(() => PhotonNetwork.Friends != null);
-        for (int i = 0; i < PhotonNetwork.Friends.Count; i++) {
-            User friend = new User();
-            for (int x =0; x < Friends.friends.Count; x++) {
-                if (Friends.friends[x].userID == PhotonNetwork.Friends[i].UserId) {
-                    friend = Friends.friends[x];
-                    break;
-                }
+        if (LocalUser.locUser.friends.Count > 0)
+        {
+            PhotonNetwork.FindFriends(LocalUser.locUser.friends.ToArray());
+
+            if (Friends.friends.Count != LocalUser.locUser.friends.Count)
+            {
+                yield return new WaitUntil(() => Friends.friends.Count == LocalUser.locUser.friends.Count);
             }
-            if (!Friends.AmIBlocked(friend)) {
-                if (PhotonNetwork.Friends[i].IsOnline) {
-                    SpawnFriendPanel(friend, true);
-                } else {
-                    SpawnFriendPanel(friend, false);
+            Debug.Log(PhotonNetwork.Friends.Count);
+            if (PhotonNetwork.Friends != null)
+            {
+                for (int i = 0; i < PhotonNetwork.Friends.Count; i++)
+                {
+                    User friend = new User();
+                    for (int x = 0; x < Friends.friends.Count; x++)
+                    {
+                        if (Friends.friends[x].userID == PhotonNetwork.Friends[i].UserId)
+                        {
+                            friend = Friends.friends[x];
+                            break;
+                        }
+                    }
+                    if (!Friends.AmIBlocked(friend))
+                    {
+                        if (PhotonNetwork.Friends[i].IsOnline)
+                        {
+                            SpawnFriendPanel(friend, true);
+                        }
+                        else
+                        {
+                            SpawnFriendPanel(friend, false);
+                        }
+                    }
                 }
             }
         }
+        LoadingScreen.Instance.TurnOffLoadingScreen();
     }
 
     void SpawnFriendPanel(User user, bool status) {
-        //if (!string.IsNullOrEmpty(user.userName) && !string.IsNullOrWhiteSpace(user.userName)) {
+        if (!string.IsNullOrEmpty(user.userName) && !string.IsNullOrWhiteSpace(user.userName)) {
             GameObject friendPanel = Instantiate(Resources.Load<GameObject>("FriendPanel"), friendsPanelParent.transform);
             friendPanel.transform.localScale = new Vector3(1, 1, 1);
             friendPanel.GetComponent<FriendListInfoPanel>().SetUpFriendPanel(user, status);
             friendPanels.Add(friendPanel);
-            //Debug.Log("WTF");
-        //}
+        }
     }
 
     private void DeleteFriends() {
